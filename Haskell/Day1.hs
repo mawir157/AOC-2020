7ca3df7d1127fbcabc432f17a7044f31e97e588a
mawir157 <jamesmthompson2@gmail.com>
2020-12-01 11:15:57 +0000

    Minor corrections to Day1.hs

--- Haskell/Day1.hs
+++ Haskell/Day1.hs
@@ -12,12 +12,14 @@ main = do
   putStrLn "Day 1"
   f <- readFile "../input/input01.txt"
   let s = map(parseInput) $ lines f
-  let t = sum s
-  let pairs = [ (x,y) | x <- [0..((length s)-1)], y <- [0..((length s)-1)]]
+  let l = (length s)-1
+
+  let pairs = [ (x,y) | x <- [0..l], y <- [0..l]]
   let sums = zipWithFn (sumBy2Indices s) pairs
   let ((v1, v2),_) = head $ dropWhile (\(x,p) -> p /= 2020) sums
   printSoln 1 ((s!!v1)*(s!!v2))
-  let triples = [ (x,y,z) | x <- [0..((length s)-1)], y <- [0..((length s)-1)], z <- [0..((length s)-1)]]
+
+  let triples = [ (x,y,z) | x <- [0..l], y <- [0..l], z <- [0..l]]
   let sums3 = zipWithFn (sumBy3Indices s) triples
   let ((w1, w2, w3),_) = head $ dropWhile (\(x,p) -> p /= 2020) sums3
   printSoln 2 ((s!!w1)*(s!!w2)*(s!!w3))

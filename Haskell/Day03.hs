f71f4fbb4db608d390fa67d3629b15e3cc0c94ea
mawir157 <jamesmthompson2@gmail.com>
2020-12-03 11:28:31 +0000

    :octopus:

--- Haskell/Day03.hs
+++ Haskell/Day03.hs
@@ -5,9 +5,9 @@ import Data.List
 parseInput :: String -> [Int]
 parseInput ss =  elemIndices '#' ss
 
-tbg :: Int -> [[Int]] -> (Int, Int) -> (Int, Int) -> Int
+tbg :: Int -> [[Int]] -> Int -> (Int, Int) -> Int
 tbg _ [] _ _ = 0
-tbg w (s:ss) (x,y) (dx, dy) = tree + (tbg w ss' (x, y') (dx,dy))
+tbg w (s:ss) y (dx, dy) = tree + (tbg w ss' y' (dx,dy))
   where tree = if' (elem y s) 1 0
         y' = (y + dy) `mod` w
         ss' = drop (dx-1) ss
@@ -18,5 +18,5 @@ main = do
   let s = map(parseInput) $ lines f
   let w = length $ head $ lines f
 
-  printSoln 1 (tbg w s (0,0) (1,3))
-  printSoln 2 (product $ map (tbg w s (0,0)) [(1,1),(1,3),(1,5),(1,7),(2,1)])
+  printSoln 1 (tbg w s 0 (1,3))
+  printSoln 2 (product $ map (tbg w s 0) [(1,1),(1,3),(1,5),(1,7),(2,1)])

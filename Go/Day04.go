80970c3c9f8281bd71edd70506303c2aac598201
mawir157 <jamesmthompson2@gmail.com>
2020-12-04 14:11:31 +0000

    Completely refactor rust code because Rust handles modules in a fucking stupid way

--- Go/Day04.go
+++ Go/Day04.go
@@ -1,45 +1,45 @@
 package main
 
 import (
-  "io/ioutil"
-  "regexp"
-  "strings"
+	"io/ioutil"
+	"regexp"
+	"strings"
 )
 
 import Helper "./adventhelper"
 
 func ParseFile(fname string) (strs []string, err error) {
-  b, err := ioutil.ReadFile(fname)
-  if err != nil { return nil, err }
+	b, err := ioutil.ReadFile(fname)
+	if err != nil { return nil, err }
 
-  lines := strings.Split(string(b), "\n")
-  temp := ""
-  for _, l := range lines {
-  	if l != "" {
-  		if len(temp) == 0 {
-  			temp = l
-  		} else {
-  			temp = temp + " " + l
-  		}
-  	} else {
-  		strs = append(strs,temp)
-  		temp = ""
-  	}
-  }
-  return strs, nil
+	lines := strings.Split(string(b), "\n")
+	temp := ""
+	for _, l := range lines {
+		if l != "" {
+			if len(temp) == 0 {
+				temp = l
+			} else {
+				temp = temp + " " + l
+			}
+		} else {
+			strs = append(strs,temp)
+			temp = ""
+		}
+	}
+	return strs, nil
 }
 
-func checkId(s string, extra bool) (part1 bool, part2 bool) {
+func checkId(s string) (part1 bool, part2 bool) {
 	part1 = true
 	part2 = true
 	ss := strings.Split(s, " ")
 
 	// a bit hacky because we don't have sets
-  seen := make(map[string]bool)
-  fields := []string{"byr", "iyr", "eyr", "hgt", "hcl", "ecl", "pid"}
-  for _, key := range fields {
-  	seen[key] = false
-  }
+	seen := make(map[string]bool)
+	fields := []string{"byr", "iyr", "eyr", "hgt", "hcl", "ecl", "pid"}
+	for _, key := range fields {
+		seen[key] = false
+	}
 
 	for _, q := range ss {
 		p := strings.Split(q, ":")
@@ -48,33 +48,31 @@ func checkId(s string, extra bool) (part1 bool, part2 bool) {
 		value := p[1]
 		seen[key] = true
 
-		if (extra) {
-			ok := true
-			switch key {
-				case "byr":
-					ok, _ = regexp.Match("(19[2-9][0-9]|200[0-2])", []byte(value))
-				case "iyr":
-					ok, _ = regexp.Match("(201[0-9]|2020)", []byte(value))
-				case "eyr":
-					ok, _ = regexp.Match("(202[0-9]|2030)", []byte(value))
-				case "hgt":
-					ok, _ = regexp.Match("(59in|6[0-9]in|7[0-6]in|1[5-8][0-9]cm|19[0-3]cm)", []byte(value))
-				case "hcl":
-					ok, _ = regexp.Match("^#([0-9a-f]){6}$", []byte(value))
-				case "ecl":
-					ok, _ = regexp.Match("(amb|blu|brn|gry|grn|hzl|oth)", []byte(value))
-				case "pid":
-					ok, _ = regexp.Match("^([0-9]){9}$", []byte(value))
-				case "cid":
-					ok = true
-			}
-			part2 = part2 && ok
+		ok := true
+		switch key {
+			case "byr":
+				ok, _ = regexp.Match("(19[2-9][0-9]|200[0-2])", []byte(value))
+			case "iyr":
+				ok, _ = regexp.Match("(201[0-9]|2020)", []byte(value))
+			case "eyr":
+				ok, _ = regexp.Match("(202[0-9]|2030)", []byte(value))
+			case "hgt":
+				ok, _ = regexp.Match("(59in|6[0-9]in|7[0-6]in|1[5-8][0-9]cm|19[0-3]cm)", []byte(value))
+			case "hcl":
+				ok, _ = regexp.Match("^#([0-9a-f]){6}$", []byte(value))
+			case "ecl":
+				ok, _ = regexp.Match("(amb|blu|brn|gry|grn|hzl|oth)", []byte(value))
+			case "pid":
+				ok, _ = regexp.Match("^([0-9]){9}$", []byte(value))
+			case "cid":
+				ok = true
 		}
+		part2 = part2 && ok
 	}
 
 	for _, key := range fields {
-  	part1 = part1 && seen[key]
-  }
+		part1 = part1 && seen[key]
+	}
 
 	return part1, part2
 }
@@ -85,7 +83,7 @@ func main() {
 	part1 := 0
 	part2 := 0
 	for _, s := range ss {
-		ok1, ok2 := checkId(s, true)
+		ok1, ok2 := checkId(s)
 		if ok1 {
 			part1 += 1
 			if ok2 {
--- Rust/run.sh
+++ Rust/run.sh
@@ -1,3 +1,5 @@
+cd aoc-2020
+
 if [ $# -gt 0 ]
 then
   day=$(printf "%02d" $1)
@@ -5,26 +7,15 @@ else
   day=-1
 fi
 
-if [ $day -gt 0 ]
+cargo run $day
+
+if [ $day -lt 0 ]
 then
-	if test -f Day$day.rs;
-	then
-		rustc Day$day.rs
-		./Day$day
-		rm Day$day
-	else
-		echo "Day " $day " does not exist"
-	fi
-else
 	missing=""
 	for i in $(seq -f "%02g" 1 25)
 	do
-		if test -f Day$i.rs;
+		if test ! -f src/day$i.rs;
 		then
-			rustc Day$i.rs > /dev/null
-			./Day$i
-			rm Day$i
-		else
 			if [ "$missing" = "" ]
 			then
 				missing=$i
@@ -38,3 +29,5 @@ else
 		echo "Missing days = ["$missing"]"
 	fi
 fi
+
+cd ..

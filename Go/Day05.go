2aca06e22fba3123e6130a61535d1601e8376ad6
mawir157 <jamesmthompson2@gmail.com>
2020-12-05 11:10:03 +0000

    do it in one :octocat:

--- Go/Day05.go
+++ Go/Day05.go
@@ -7,19 +7,14 @@ import (
 
 import Helper "./adventhelper"
 
-func binSearch(v string, s string, l int) (n int) {
+func getSeat(ones string, s string, l int) (n int) {
 	n = 0
-	vv := strings.Split(v, "")
 	for i, c := range s {
-			if (string(c) == vv[1]) {
-	    	n += Helper.PowInt(2, l - 1 - i)
-	  	}
+		if strings.Contains(ones, string(c)) {
+			n += Helper.PowInt(2, l - 1 - i)
+		}
 	}
 	return n
-}
-
-func getSeat(s string) (n int) {
-	return (8 * binSearch("FB", s[:7], 7)) + binSearch("LR", s[7:], 3)
 }
 
 func missing(is []int) (n int) {
@@ -35,7 +30,7 @@ func main() {
 	ss, _ := Helper.ReadStrFile("../input/input05.txt")
 	var seats []int
 	for _,s := range(ss) {
-		seats = append(seats, getSeat(s))
+		seats = append(seats, getSeat("BR", s, 10))
 	}
 	sort.Ints(seats[:])
 
--- Haskell/Day05.hs
+++ Haskell/Day05.hs
@@ -2,23 +2,18 @@ import AdventHelper
 
 import Data.List
 
-binSearch :: String -> String -> Integer
-binSearch [c0,c1] s = sum $ zipWith (*) b $ zipWith (^) (repeat 2) [0..6]
-  where b = reverse $ map (\x -> if' (x == c0) 0 1) s
-
-getSeat :: String -> Integer
-getSeat s = (8 * r) + c
-  where r = binSearch "FB" (take 7 s)
-        c = binSearch "LR" (drop 7 s)
+getSeat :: String -> String -> Integer
+getSeat ones s = sum $ zipWith (*) b $ zipWith (^) (repeat 2) [0..(length s - 1)]
+  where b = reverse $ map (\x -> if' (elem x ones) 0 1) s 
 
 missing :: [Integer] -> Integer
 missing [x] = x
-missing (x:y:xs) = if' (x + 1 /= y) (x+1) (missing (y:xs))
+missing (x:y:xs) = if' (succ x /= y) (succ x) (missing (y:xs))
 
 main = do
   putStrLn "Day 5"
   f <- readFile "../input/input05.txt"
-  let s = map (getSeat) $ lines f
+  let s = map (getSeat "FL") $ lines f
 
   printSoln 1 (maximum s)
   printSoln 2 (missing $ sort s)
